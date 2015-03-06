bae359e1633815371dcaaca504237a688f9727be
DiamondIceNS <fawfulcopter@gmail.com>
2015-03-06 15:46:05 -0600

    Beginnings of Pause screen

---
+++ Dauntlet/Dauntlet/Dauntlet/Dauntlet/PauseScreen.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Dauntlet
+{
+    public class PauseScreen : GameScreen
+    {
+        public PauseScreen(Dauntlet game) : base(game)
+        {
+        }
+
+        public override void LoadContent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UnloadContent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Draw(GameTime gametime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
