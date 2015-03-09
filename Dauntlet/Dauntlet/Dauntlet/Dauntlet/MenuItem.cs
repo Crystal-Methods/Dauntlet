848477e34e0d9c6df2511d042a497724670783d1
DiamondIceNS <fawfulcopter@gmail.com>
2015-03-08 22:13:56 -0500

    Committing code just so I can pull latest sync

---
+++ Dauntlet/Dauntlet/Dauntlet/Dauntlet/MenuItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Dauntlet
+{
+    class MenuItem
+    {
+        public bool Selected;
+        public String Text { get; set; }
+        public Vector2 Position { get; set; }
+
+        public MenuItem(string text)
+        {
+            Text = text;
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            
+        }
+
+    }
+}
