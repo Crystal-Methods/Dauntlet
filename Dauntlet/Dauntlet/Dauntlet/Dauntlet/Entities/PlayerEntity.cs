81b6dedb5f6bcfb2744ffcb1c245b2811e4c0871
Tyler-Losinski <tyler.a.losinski@ndsu.edu>
2015-03-25 16:12:27 -0500

    Fix a few things with moving

--- Dauntlet/Dauntlet/Dauntlet/Dauntlet/Entities/PlayerEntity.cs
+++ Dauntlet/Dauntlet/Dauntlet/Dauntlet/Entities/PlayerEntity.cs
@@ -13,6 +13,7 @@ namespace Dauntlet.Entities
 {
     public class PlayerEntity : Entity
     {
+        private const float speed = 15f;
         private const float radius = 15f; // Radius of player's bounding circle
         private const float defaultOffGroundHeight = 10f; // How far the base of the sprite is from the center of the shadow
         private const float mass = 1f;
@@ -50,6 +51,7 @@ namespace Dauntlet.Entities
             CollisionBody.FixedRotation = true;
             CollisionBody.Restitution = 0;
             CollisionBody.Friction = 0.5f;
+            CollisionBody.LinearDamping = 25f;
             CollisionBody.AngularDamping = 100f;
             float m = CollisionBody.Mass;
 
