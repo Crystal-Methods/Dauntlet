7dc6c327511495efc4b15f15760880f03cebc26a
Gogurt <garrett.leatherman@ndsu.edu>
2015-04-09 00:49:38 -0500

    Health Stock: Maximum Pulsation Edition
    The current sprite is way too distracting in my opinion. I might tone
    down the bounciness later on.

--- Dauntlet/Dauntlet/Dauntlet/Dauntlet/HUD.cs
+++ Dauntlet/Dauntlet/Dauntlet/Dauntlet/HUD.cs
@@ -20,15 +20,19 @@ namespace Dauntlet
             _flame.AddAnimation("Flicker1", 0, 0, 256, 256, 7, 1/10f, false);
             _flame.SetAnimation("Flicker1");
             _healthStock = new AnimatedTexture2D(SpriteFactory.GetTexture("HealthStock"));
+            _healthStock.AddAnimation("healthStock",0,0,64,64,10,1/17f,false);
+            _healthStock.SetAnimation("healthStock");
 
             _hsa = new Vector2(-(float) Math.Cos(22.38), (float) Math.Sin(22.38));
             _hsa.Normalize();
-            _hsa *= 20;
+            _hsa *= 40;
         }
 
         public static void Update(GameTime gameTime)
         {
             _flame.StepAnimation(gameTime);
+            _healthStock.StepAnimation(gameTime);
+            
         }
 
         public static void Draw(SpriteBatch spriteBatch)
@@ -37,7 +41,9 @@ namespace Dauntlet
             spriteBatch.Draw(_flame.Sheet, new Vector2(83, 151), _flame.CurrentFrame, Color.White, 0f, new Vector2(132, 194), 1f, SpriteEffects.None, 0f);
             for (int i = 0; i < GameplayScreen.Player.Health; i++)
             {
-                spriteBatch.Draw(_healthStock.Sheet, new Vector2(140, 102) + (_hsa * i), _healthStock.CurrentFrame, Color.White, 0f, new Vector2(10, 10), 1f, SpriteEffects.None, 1f);
+                _healthStock.Frame += i;
+                spriteBatch.Draw(_healthStock.Sheet, new Vector2(100, 70) + (_hsa * i), _healthStock.CurrentFrame, Color.White, 0f, new Vector2(10, 10), 1f, SpriteEffects.None, 0f);
+
             }
         }
     }
