96082a591643fce3ebaf87eeb313f739059dd9e4
Gogurt <garrett.leatherman@ndsu.edu>
2015-04-16 01:36:17 -0500

    Main Menu Update
    Clouds are scrolling. Options have been added but it isn't connected to
    anything (If we decide to add options that is, I'm just testing the
    scrolling system. It's kind of broken still.)

--- Dauntlet/Dauntlet/Dauntlet/Dauntlet/GameScreens/MainMenuScreen.cs
+++ Dauntlet/Dauntlet/Dauntlet/Dauntlet/GameScreens/MainMenuScreen.cs
@@ -10,11 +10,18 @@ namespace Dauntlet.GameScreens
     {
         private ContentManager _content;
         private SpriteBatch _spriteBatch;
-        private Texture2D _bgTex;
+        private Texture2D _bgTex_sky;
+        private Texture2D _bgTex_clouds_1;
+        private Texture2D _bgTex_clouds_2;
+        private Texture2D _bgTex_house;
+        private Texture2D _bgTex_title;
+        private Texture2D _bgTex_options;
         private Texture2D _fistTex;
         private Texture2D _black;
 
         private int _menuSelection;
+        private Vector2 cloudPos1;
+        private Vector2 cloudPos2;
 
         private Cue _menuTheme;
 
@@ -31,7 +38,13 @@ namespace Dauntlet.GameScreens
             if (_content == null) _content = new ContentManager(MainGame.Services, "Content");
             if (_spriteBatch == null) _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            _bgTex = _content.Load<Texture2D>("Textures/Dauntlet");
+            _bgTex_sky = _content.Load<Texture2D>("Textures/Menu/Menu_Sky");
+            _bgTex_clouds_1 = _content.Load<Texture2D>("Textures/Menu/Menu_Clouds");
+            _bgTex_clouds_2 = _content.Load<Texture2D>("Textures/Menu/Menu_Clouds");
+            _bgTex_house = _content.Load<Texture2D>("Textures/Menu/Menu_House");
+            _bgTex_title = _content.Load<Texture2D>("Textures/Menu/Menu_Title");
+            _bgTex_options = _content.Load<Texture2D>("Textures/Menu/Menu_Options");
+
             _fistTex = _content.Load<Texture2D>("Textures/Fist");
 
             int width = MainGame.Graphics.Viewport.Width;
@@ -42,6 +55,10 @@ namespace Dauntlet.GameScreens
             _black.SetData(data);
 
             _menuSelection = 0;
+
+            cloudPos1 = new Vector2(0, 0);
+            cloudPos2 = new Vector2(1366, 0);
+
 
             //SoundManager.PlaySong("MainTheme");
             //SoundManager.VolumeChange(0.0f);
@@ -70,6 +87,10 @@ namespace Dauntlet.GameScreens
                 }
                 else if(_menuSelection == 1)
                 {
+
+                }
+                else if(_menuSelection == 2)
+                {
                     MainGame.Exit();
                 }
                 //Additional options to be added here.
@@ -78,7 +99,7 @@ namespace Dauntlet.GameScreens
                 MainGame.Exit();
             if(MainGame.Input.IsMenuDown())
             {
-                if(_menuSelection < 1)
+                if(_menuSelection < 2)
                 {
                     _menuSelection++;
                     //SoundManager.Play("MenuBlip");
@@ -94,16 +115,33 @@ namespace Dauntlet.GameScreens
                     Dauntlet.SoundBank.PlayCue("MenuBlip");
                 }
             }
+
+            cloudPos1.X -= 0.20f;
+            cloudPos2.X -= 0.20f;
+            if(cloudPos1.X == -1366)
+            {
+                cloudPos1.X = 1336;
+            }
+            if (cloudPos2.X == -1366)
+            {
+                cloudPos2.X = 1366;
+            }
         }
 
         public override void Draw(GameTime gametime)
         {
-            float drawScale = (float) MainGame.Graphics.Viewport.Height/_bgTex.Height;
-            var translate = new Vector2((MainGame.Graphics.Viewport.Width - _bgTex.Width*drawScale)/2f, 0);
+            float drawScale = (float) MainGame.Graphics.Viewport.Height/_bgTex_sky.Height;
+            var translate = new Vector2((MainGame.Graphics.Viewport.Width - _bgTex_sky.Width*drawScale)/2f, 0);
 
             _spriteBatch.Begin();
             _spriteBatch.Draw(_black, Vector2.Zero, Color.White);
-            _spriteBatch.Draw(_bgTex, translate, null, Color.White, 0f, Vector2.Zero, drawScale, SpriteEffects.None, 1f);
+            _spriteBatch.Draw(_bgTex_sky, translate, null, Color.White, 0f, Vector2.Zero, drawScale, SpriteEffects.None, 1f);
+            _spriteBatch.Draw(_bgTex_clouds_1, cloudPos1, null, Color.White, 0f, Vector2.Zero, drawScale, SpriteEffects.None, 1f);
+            _spriteBatch.Draw(_bgTex_clouds_2, cloudPos2, null, Color.White, 0f, Vector2.Zero, drawScale, SpriteEffects.None, 1f);
+            _spriteBatch.Draw(_bgTex_house, translate, null, Color.White, 0f, Vector2.Zero, drawScale, SpriteEffects.None, 1f);
+            _spriteBatch.Draw(_bgTex_title, translate, null, Color.White, 0f, Vector2.Zero, drawScale, SpriteEffects.None, 1f);
+            _spriteBatch.Draw(_bgTex_options, translate, null, Color.White, 0f, Vector2.Zero, drawScale, SpriteEffects.None, 1f);
+
 
             // Pulsate the size of the selected menu entry.
             double time = gametime.TotalGameTime.TotalSeconds;
@@ -122,6 +160,10 @@ namespace Dauntlet.GameScreens
             else if(_menuSelection == 1)
             {
                 position = new Vector2(450, 625) * drawScale;
+            }
+            else if(_menuSelection == 2)
+            {
+                position = new Vector2(450, 690) * drawScale;
             }
 
             _spriteBatch.Draw(_fistTex, position + translate, null, Color.White, 0f, origin, scale, SpriteEffects.None, 1f);
